75528c3d3b5104906346e1adb704f662a058a6a1
azmcfann <64863102+azmcfann@users.noreply.github.com>
2020-07-24 09:28:29 -0500

    attempted push and fixx
    attempt 1 to fix the problems

---
+++ Managers/PlotManager.h
@@ -0,0 +1,19 @@
+#pragma once
+// Authors:
+//      Original python code created by
+//      Nathaniel Goldfarb
+//      
+//      C++ code adapted from the python code by
+//      Alexandra McFann
+#ifndef PLOTMANAGER_H_
+#define PLOTMANAGER_H_
+#include "Manager.h"
+class PlotManager : public Manager {
+public:
+	PlotManager();
+	virtual ~PlotManager();
+private:
+
+};
+
+#endif /* PLOTMANAGER_H_ */
---
+++ Managers/RecorderManager.h
@@ -0,0 +1,19 @@
+// Authors:
+//      Original python code created by
+//      Nathaniel Goldfarb
+//      
+//      C++ code adapted from the python code by
+//      Alexandra McFann
+#ifndef RECORDERMANAGER_H_
+#define RECORDERMANAGER_H_
+#include "Manager.h"
+
+class RecorderManager : public Manager {
+public:
+	RecorderManager();
+	virtual ~RecorderManager();
+private:
+
+};
+
+#endif /* RECORDERMANAGER_H_ */
